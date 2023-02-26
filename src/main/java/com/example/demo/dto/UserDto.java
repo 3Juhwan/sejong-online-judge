e062c54f1095cb716f85d2efcadadac5fe15e455
3juhwan <13selfesteem91@naver.com>
2023-02-26 21:03:33 +0900

    feat(soj): 유저DTO 구현

--- src/main/java/com/example/demo/dto/UserDto.java
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -4,40 +4,42 @@ import com.example.demo.entity.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 
 @Getter
-@Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDto {
 
     @NotNull
-    @Size(min = 3, max = 50)
+    @Column(unique = true, updatable = false)
+    @Pattern(regexp = "^[a-z0-9]{5,10}$",
+            message = "닉네임은 특수문자를 제외한 5~10자리여야 합니다.")
+    @Size(min = 5, max = 10)
     private String username;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull
-    @Size(min = 3, max = 100)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Size(min = 8, max = 16)
     private String password;
 
     @NotNull
-    @Size(min = 3, max = 50)
-    private String nickname;
-
-    private String authority;
+    @Email(message = "이메일 형식을 확인해주세요.")
+    @Size(max = 30)
+    private String email;
 
     public static UserDto from(User user) {
-        if(user == null) return null;
+        if (user == null) return null;
 
         return UserDto.builder()
                 .username(user.getUsername())
-                .nickname(user.getNickname())
-                .authority(user.getAuthority())
+                .email(user.getEmail())
                 .build();
     }
-
 }
