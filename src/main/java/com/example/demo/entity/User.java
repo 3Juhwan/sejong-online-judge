2a125e0301751c49491da723d43ecea408c777eb
3juhwan <13selfesteem91@naver.com>
2023-02-26 21:01:31 +0900

    fix(soj): 패스워드 유효성 검사 수정

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -12,7 +12,6 @@ import javax.validation.constraints.Size;
 @Table(name = "user")
 @Entity
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,25 +24,22 @@ public class User extends BaseTime {
 
     @NotNull
     @Column(unique = true, updatable = false)
-    @Pattern(regexp = "^[a-z0-9]{5,10}$",
-            message = "닉네임은 특수문자를 제외한 5~10자리여야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]{5,10}$")
     @Size(min = 5, max = 10)
     private String username;
 
     @NotNull
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
-            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
-    @Size(min = 8, max = 16)
     private String password;
 
     @NotNull
-    @Email(message = "이메일 형식을 확인해주세요.")
+    @Email
     @Size(max = 30)
     private String email;
 
     @NotNull
     private String authority;
 
+    @NotNull
     private boolean activated;
 
 }
