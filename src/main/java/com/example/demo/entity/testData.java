507bd2f0c3aec330cf8a14ee77f4a3b00a4a5e18
3juhwan <13selfesteem91@naver.com>
2023-02-26 21:42:47 +0900

    feat(soj): 테스트 데이터 엔티티 구현

---
+++ src/main/java/com/example/demo/entity/testData.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import com.example.demo.entity.util.BaseTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class testData extends BaseTime {
+
+    @Id
+    @Column(name = "test_data_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "problem_id")
+    private Problem problem;
+
+    private String input;
+
+    private String output;
+}
