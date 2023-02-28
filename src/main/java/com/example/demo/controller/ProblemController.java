9b812e01a46c41f3669307d25d0c09910ce2b00c
3juhwan <13selfesteem91@naver.com>
2023-03-01 00:11:29 +0900

    feat(soj): 문제 컨트롤러 일부 구현

--- src/main/java/com/example/demo/controller/ProblemController.java
+++ src/main/java/com/example/demo/controller/ProblemController.java
@@ -4,9 +4,14 @@ import com.example.demo.dto.ProblemDto;
 import com.example.demo.service.ProblemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.util.Objects;
+
+import static com.example.demo.util.AuthUtil.studentExclusiveAuth;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -16,8 +21,22 @@ public class ProblemController {
     private final ProblemService problemService;
 
     @PostMapping("/new-problem")
-//    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @PreAuthorize(studentExclusiveAuth)
     public ResponseEntity<ProblemDto> addProblem(@Valid @RequestBody ProblemDto problemDto) {
         return ResponseEntity.ok(problemService.saveProblem(problemDto));
     }
+
+    @PostMapping("/problem/{id}/update")
+    @PreAuthorize(studentExclusiveAuth)
+    public ResponseEntity<ProblemDto> updateProblem(@Valid @RequestBody ProblemDto problemDto) {
+        return ResponseEntity.ok(problemService.updateProblem(problemDto));
+    }
+
+    @GetMapping("/problem/{id}/delete")
+    @PreAuthorize(studentExclusiveAuth)
+    public ResponseEntity<Objects> deleteProblem(@PathVariable("id") Long problemId) {
+        problemService.deleteProblem(problemId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
