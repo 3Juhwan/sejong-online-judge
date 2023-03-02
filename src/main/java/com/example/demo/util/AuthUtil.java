5a62778d5e57bff2bdfb8a22941d0c6f89763851
3juhwan <13selfesteem91@naver.com>
2023-03-02 13:00:34 +0900

    feat(auth-util): 새로운 권한 등록

--- src/main/java/com/example/demo/util/AuthUtil.java
+++ src/main/java/com/example/demo/util/AuthUtil.java
@@ -3,4 +3,5 @@ package com.example.demo.util;
 public class AuthUtil {
 
     public static final String studentExclusiveAuth = "hasAnyRole('TA', 'PROFESSOR', 'ADMIN')";
+    public static final String adminOnlyAuth = "hasAnyRole('ADMIN')";
 }
