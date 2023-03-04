7769963cf2375ab1bfb260fa6ccce583ec64096a
3juhwan <13selfesteem91@naver.com>
2023-03-04 23:26:14 +0900

    feat(contest-entity): 콘테스트 엔티티 초안 개발

---
+++ src/main/java/com/example/demo/entity/Contest.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import com.example.demo.entity.util.BaseTime;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Entity
+public class Contest extends BaseTime {
+
+    @Id
+    @Column(name = "contest_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @OneToMany(mappedBy = "problem")
+    private List<Problem> contestProblems;
+}
