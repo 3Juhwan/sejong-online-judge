cfb494101231df400a48678a6cfd5826577f7e5d
3juhwan <13selfesteem91@naver.com>
2023-03-05 08:48:13 +0900

    feat(auth-util): 모든 종류 권한 추가

--- src/main/java/com/example/demo/util/AuthUtil.java
+++ src/main/java/com/example/demo/util/AuthUtil.java
@@ -2,6 +2,8 @@ package com.example.demo.util;
 
 public class AuthUtil {
 
+    public static final String allAuth = "hasAnyRole('STUDENT', 'TA', 'PROFESSOR', 'ADMIN')";
     public static final String studentExclusiveAuth = "hasAnyRole('TA', 'PROFESSOR', 'ADMIN')";
+    public static final String professorAuth = "hasAnyRole('PROFESSOR', 'ADMIN')";
     public static final String adminOnlyAuth = "hasAnyRole('ADMIN')";
 }
