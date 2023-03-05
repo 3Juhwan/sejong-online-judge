fe406408aae96d093722ebbf157becf767402ea4
3juhwan <13selfesteem91@naver.com>
2023-03-05 23:53:21 +0900

    chore(user-controller): 유저 컨트롤러 URL 수정

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,21 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.user.UserDto;
 import com.example.demo.service.UserService;
-import com.example.demo.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping("/signup")
     public ResponseEntity<UserDto> signup(@Valid @RequestBody UserDto userDto) {
@@ -23,13 +24,15 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public ResponseEntity<UserDto> getMyUserInfo() {
-        return ResponseEntity.ok(userService.getMyUserWithAuthority());
+    @PreAuthorize("hasAnyRole('TA')")
+    public ResponseEntity<?> myInfo() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/"));
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
-    @GetMapping("/{username}")
-    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('TA')")
     public ResponseEntity<UserDto> getUserInfo(@PathVariable String username) {
         return ResponseEntity.ok(userService.getUserWithAuthority(username));
     }
