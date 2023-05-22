eaa7942073c595e87e46a927417f0d9717aa7936
3juhwan <13selfesteem91@naver.com>
2023-05-23 00:52:03 +0900

    fix(post): response body 수정

--- src/main/java/com/example/demo/dto/postBox/PostBoxResponseDto.java
+++ src/main/java/com/example/demo/dto/postBox/PostBoxResponseDto.java
@@ -1,7 +1,9 @@
 package com.example.demo.dto.postBox;
 
+import com.example.demo.entity.Post;
 import com.example.demo.entity.PostBox;
 import lombok.*;
+import org.springframework.security.core.parameters.P;
 
 import java.time.LocalDateTime;
 
@@ -17,21 +19,27 @@ public class PostBoxResponseDto {
     private String courseName;
     private String contestName;
     private String contestProblemName;
+    private Long contestProblemId;
     private String coverContent;
     private Boolean finished;
     private LocalDateTime modifiedTime;
 
     public static PostBoxResponseDto from(PostBox postBox) {
-        return PostBoxResponseDto.builder()
+        PostBoxResponseDtoBuilder postBoxResponseDtoBuilder = PostBoxResponseDto.builder()
                 .postBoxId(postBox.getId())
                 .username(postBox.getUser().getUsername())
                 .courseName(postBox.getCourse().getTitle())
                 .contestName(postBox.getContest().getTitle())
                 .contestProblemName(postBox.getContestProblem().getTitle())
-                .coverContent(postBox.getLastPost().getContent())
+                .contestProblemId(postBox.getContestProblem().getId())
                 .finished(postBox.getFinished())
-                .modifiedTime(postBox.getModifiedTime())
-                .build();
+                .modifiedTime(postBox.getModifiedTime());
+        Post lastPost = postBox.getLastPost();
+        if(lastPost != null) {
+            postBoxResponseDtoBuilder.coverContent(lastPost.getContent());
+        }
+        return postBoxResponseDtoBuilder.build();
+
     }
 }
 
