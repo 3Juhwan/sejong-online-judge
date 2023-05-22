f84d23d4a9c1896d5f661a0dbf9e3b301c865e58
3juhwan <13selfesteem91@naver.com>
2023-05-22 22:34:03 +0900

    refactor(main): 불필요한 import 제거

--- src/main/java/com/example/demo/SojApplication.java
+++ src/main/java/com/example/demo/SojApplication.java
@@ -1,18 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class SojApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SojApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SojApplication.class, args);
+    }
 
 }
